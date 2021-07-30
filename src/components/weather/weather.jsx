fad159b910a2eaee22c84044719dd85da9cf1e0f
buttman445 <amirbaurzhanov@gmail.com>
2021-07-30 22:52:50 +0600

    commit

--- src/components/weather/weather.jsx
+++ src/components/weather/weather.jsx
@@ -3,10 +3,10 @@ import WeatherForm from "./weatherForm";
 const Weather = (props) => {
     return (
         <div className="container pt-5">
-            <h4 className="pb-1">There you can find your city weather!</h4>
+            <h4 className="pb-1">Here you can find your city weather!</h4>
             <WeatherForm onSubmit={props.onSubmit} />
             {!props.weather.wTemp ? <div className="pt-5"> There nothing </div> : <div className="pt-5">
-                <div className="col-4 offset-1">
+                <div>
                     <h1>Information </h1>
                     <div className="alert alert-success">
                         <div className="row">
